99543377a3d5df2039db9383f3ee8a4bc88f51e4
Luciano Ramalho <luciano@ramalho.org>
2013-12-13 13:43:33 -0200

    download completo exceto palavras com hifen coincidindo com o final do prefixo

--- vocab.py
+++ vocab.py
@@ -4,25 +4,39 @@ import aiohttp
 import string
 import json
 import os
+import glob
 
 BASE = ('http://www.academia.org.br/sistema_busca_palavras_portuguesas'
         '/volta_voca_org.asp?palavra=')
 
+TIMEOUT = 300
+
 @asyncio.coroutine
 def baixar(prefixo):
+    letra = prefixo[0]
+    caminho = 'data/%s/%s' % (letra, prefixo)
+    if glob.glob(caminho+'*'):
+        print('OK', caminho)
+        return
     busca = prefixo + '*'
-    response = yield from aiohttp.request('GET', BASE+busca)
-    data = yield from response.read()
-    html = data.decode('cp1252')
-    lista, continua = extrair(html)
-    if lista:
-        sufixo = '-' if continua else ''
-        letra = prefixo[0]
-        os.makedirs('data/' + letra, exist_ok=True)
-        nome_arq = 'data/%s/%s%s.json' % (letra, prefixo, sufixo)
-        with open(nome_arq, 'wt', encoding='utf-8') as saida:
-            json.dump(lista, saida)
-        print(nome_arq, 'salvo')
+    response = yield from aiohttp.request('GET', BASE+busca, timeout=TIMEOUT,
+                                            conn_timeout=TIMEOUT)
+    print(prefixo, response.status, end=' ')
+    if response.status == 200:
+        data = yield from response.read()
+        html = data.decode('cp1252')
+        lista, continua = extrair(html)
+        if 1 or lista:
+            sufixo = '-' if continua else ''
+            os.makedirs('data/' + letra, exist_ok=True)
+            nome_arq = 'data/%s/%s%s.json' % (letra, prefixo, sufixo)
+            with open(nome_arq, 'wt', encoding='utf-8') as saida:
+                json.dump(lista, saida)
+            print('--->', nome_arq, 'salvo')
+        else:
+            print('nada')
+    else:
+        print()
 
 def extrair(html) -> (list, bool):
     """extrair lista de vocábulos do html, e indicador de continuação"""
@@ -46,6 +60,7 @@ def extrair(html) -> (list, bool):
 
 
 def principal():
+    from vocab_passo2 import gerar_prefixos
     tarefas = []
     """
     for prefixo in [c1+c2+c3 for c1 in string.ascii_lowercase
@@ -53,7 +68,8 @@ def principal():
                              for c3 in string.ascii_lowercase
                    ]:
     """
-    for prefixo in string.ascii_lowercase:
+    #for prefixo in string.ascii_lowercase: #gerar_prefixos(1):
+    for prefixo in gerar_prefixos(8):
         tarefas.append(baixar(prefixo))
 
     loop = asyncio.get_event_loop()
--- vocab_passo2.py
+++ vocab_passo2.py
@@ -1,8 +1,30 @@
 import json
 import glob
+import unicodedata
+import string
 
-for nome_arq in sorted(glob.glob('data/?/???-.json')):
-    with open(nome_arq) as entrada:
-        lista = json.load(entrada)
-    print(nome_arq.split('.')[0], lista[-1])
+def remover_acentos(txt, codif='utf-8'):
+    return unicodedata.normalize('NFKD', txt).encode('ASCII','ignore').decode('ASCII')
+
+"""
+bio-historiador
+oxi-hidrila
+pau-de-bicho
+"""
+
+TAM_PREFIX = 4
+
+def gerar_prefixos(tamanho=TAM_PREFIX):
+    interr = '?' * (tamanho-1)
+    for nome_arq in sorted(glob.glob('data/?/%s-.json' % interr)):
+        with open(nome_arq) as entrada:
+            lista = json.load(entrada)
+        ultima = lista[-1]
+        prefixo = remover_acentos(ultima[:tamanho])
+        indice = string.ascii_lowercase.find(prefixo[-1])
+        if indice >= 0:
+            finais = string.ascii_lowercase[indice:]
+            #print(prefixo[:3], finais)
+            for letra in finais:
+                yield prefixo[:tamanho-1] + letra
 
